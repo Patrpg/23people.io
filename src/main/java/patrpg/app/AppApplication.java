9e4439514f1c4303f351f78c70601a7cd8ead70c
Patrpg <patricio.pinog@gmail.com>
2020-09-01 19:27:10 -0400

    git init

---
+++ src/main/java/patrpg/app/AppApplication.java
@@ -0,0 +1,16 @@
+package patrpg.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.SpringVersion;
+
+@SpringBootApplication
+public class AppApplication {
+
+    // Spring Framework 5.2.8.RELEASE
+    // Spring Boot 2.3.3.RELEASE
+    public static void main(String[] args) {
+        SpringApplication.run(AppApplication.class, args);
+    }
+
+}
