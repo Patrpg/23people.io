7d8fa75415aec9842471c91825fc11f408a6096a
Pato <pato@dynae.tech>
2020-09-01 19:27:55 -0400

    unused import remove

--- src/main/java/patrpg/app/AppApplication.java
+++ src/main/java/patrpg/app/AppApplication.java
@@ -2,7 +2,6 @@ package patrpg.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.SpringVersion;
 
 @SpringBootApplication
 public class AppApplication {
