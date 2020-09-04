434b05cc62e457ce14c547ed8b2557c143dc9936
Patrpg <patricio.pinog@gmail.com>
2020-09-03 22:03:17 -0400

    add endpoints guidelines and methods.

--- src/main/java/patrpg/app/controller/CourseController.java
+++ src/main/java/patrpg/app/controller/CourseController.java
@@ -1,9 +1,66 @@
 package patrpg.app.controller;
 
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import patrpg.app.model.Course;
+import patrpg.app.service.CourseService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
 public class CourseController {
+
+    CourseService courseService;
+
+    /*
+    GET /courses -> It returns a paginated list of existing courses
+     */
+    @GetMapping("/courses")
+    public ResponseEntity<List<Course>> getCourses() {
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.OK);
+    }
+
+    /*
+    GET /courses/:id -> It returns the course identified by the given id. A Not Found 404
+    response status must be returned if there is no course with the given :id
+     */
+    @GetMapping("/courses/{id}")
+    public ResponseEntity<Course> getCourseById(@PathVariable Integer id) {
+        return new ResponseEntity<>(new Course(), HttpStatus.OK);
+    }
+
+    /*
+    POST /courses -> Create a course with the data provided in the json body request.
+    A Bad Request 400 response status must be returned if the body request json is
+    invalid.
+     */
+    @PostMapping("/courses")
+    public ResponseEntity<Course> insertCourse(@RequestBody Course course) {
+        return new ResponseEntity<>(new Course(), HttpStatus.CREATED);
+    }
+
+    /*
+    PUT /courses/:id -> Update a course with the given id and the body request json. A
+    Not Found 404 response status must be returned if there is no course with the
+    given :id. A Bad Request 400 response status must be returned if the body request
+    json is invalid.
+     */
+    @PutMapping("/courses/{id}")
+    public ResponseEntity<Course> updateCourse(@PathVariable Integer id,
+                                               @RequestBody Course course) {
+        return new ResponseEntity<>(new Course(), HttpStatus.OK);
+    }
+
+    /*
+    DELETE /courses/:id -> Delete a course with the given id. A Not Found 404
+    response status must be returned if there is no course with the given :id
+     */
+    @DeleteMapping("/courses/{id}")
+    public ResponseEntity<Course> deleteCourse(@PathVariable Integer id) {
+        return new ResponseEntity<>(new Course(), HttpStatus.NO_CONTENT);
+    }
 }
--- src/main/java/patrpg/app/controller/StudentController.java
+++ src/main/java/patrpg/app/controller/StudentController.java
@@ -1,9 +1,67 @@
 package patrpg.app.controller;
 
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import patrpg.app.model.Student;
+import patrpg.app.service.StudentService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
 public class StudentController {
+
+    StudentService SudentService;
+
+    /*
+    GET /students -> It returns a paginated list of existing students
+     */
+    @GetMapping("/students")
+    public ResponseEntity<List<Student>> getStudents() {
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.OK);
+    }
+
+    /*
+    GET /students/:id -> It returns the student identified by the given id. A Not Found 404
+    response status must be returned if there is no student with the given :id
+     */
+    @GetMapping("/students/{id}")
+    public ResponseEntity<Student> getStudentById(@PathVariable Integer id) {
+        return new ResponseEntity<>(new Student(), HttpStatus.OK);
+    }
+
+    /*
+    POST /Students -> Create a student with the data provided in the json body request.
+    A Bad Request 400 response status must be returned if the body request json is
+    invalid.
+     */
+    @PostMapping("/students")
+    public ResponseEntity<Student> insertStudent(@RequestBody Student student) {
+        return new ResponseEntity<>(new Student(), HttpStatus.CREATED);
+    }
+
+    /*
+    PUT /students/:id -> Update a student with the given id and the body request json. A
+    Not Found 404 response status must be returned if there is no student with the
+    given :id. A Bad Request 400 response status must be returned if the body request
+    json is invalid.
+     */
+    @PutMapping("/students/{id}")
+    public ResponseEntity<Student> updateStudent(@PathVariable Integer id,
+                                                 @RequestBody Student student) {
+        return new ResponseEntity<>(new Student(), HttpStatus.OK);
+    }
+
+    /*
+    DELETE /students/:id -> Delete a student with the given id. A Not Found 404
+    response status must be returned if there is no student with the given :id
+     */
+    @DeleteMapping("/students/{id}")
+    public ResponseEntity<Student> deleteStudent(@PathVariable Integer id) {
+        return new ResponseEntity<>(new Student(), HttpStatus.NO_CONTENT);
+    }
+
 }
